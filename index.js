91c7613ffb5bd9c044bcd91c70d09b314eeef1c1
Severity Codes <serenitysmithrn@hotmail.com>
2024-08-10 20:54:35 +0000

    code challenges

---
+++ index.js
@@ -0,0 +1,8 @@
+let side = 3;
+
+let area = side * side;
+let perimeter = 4 * side;
+
+console.log("Area = " + area);
+console.log("Perimeter = " + perimeter);
+//print perimeter and area of a square 
